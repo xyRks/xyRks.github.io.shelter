cec37afa9175f450d0792c9629708dbae1891e17
xyRks <52831378+xyRks@users.noreply.github.com>
2021-04-22 21:02:53 +0300

    add

---
+++ scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    let position = 0;
+    const slidesToShow = 3;
+    const slidesToScroll = 1;
+
+    const container = $('.frends-content');
+    const track = $('.frends-content-slider');
+    const item  = $('.frends-content-card');
+    const buttonPrev = $('.prev');
+    const buttonNext = $('.next');
+    const itemsCount = item.lenght;
+    
+
+    const itemWidth = (container.width() / slidesToShow) - 10;
+    //  const itemWidth = 270;
+    const movePosition = slidesToScroll * itemWidth;
+    
+        item.each(function (index, item) {
+            $(track).css ({
+                minWidth: track,
+            });
+        });
+        
+
+    buttonPrev.click(function() {
+        const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+        position += movePosition;
+
+        setPosition()
+        checkBtns()
+    });
+
+    buttonNext.click(function() {
+        const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+        position -= movePosition;
+        
+        setPosition()
+        checkBtns()
+    });
+
+    const setPosition = () => {
+
+        item.css({
+            transform: `translateX(${position}px)`
+        });
+    };
+
+    const checkBtns = () => {
+        buttonPrev.prop('disabled', position === 0);
+        buttonNext.prop('disabled', position <= -(itemsCount - slidesToShow) * itemWidth
+        );
+    };
+
+    checkBtns()
+});
